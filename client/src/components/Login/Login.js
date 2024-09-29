2074b83d929155f0d87289ba514bf8c007f891cb
RoyalFlush1010 <nishant.dumbre@gmail.com>
2024-09-29 05:53:04 +0530

    Created signup, login and forgot pwd page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Form, InputGroup, Row } from 'react-bootstrap'
 import classes from './Login.module.css'
 import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-
+  const [pageType, setpageType] = useState('login')
+  const loginToggeler = (condition) => {
+    setpageType(condition)
+  }
 
   return (
     <Container fluid className={classes.container}>
       <div><Toaster position="top-center" reverseOrder={false} /></div>
       <div className={classes.containerBox}>
-        <h1 className={classes.heading}>Login</h1>
+        {pageType === 'login' && <h1 className={classes.heading}>Login</h1>}
+        {pageType === 'signup' && <h1 className={classes.heading}>Signup</h1>}
+        {pageType === 'reset-form' && <h1 className={classes.heading}>Enter email</h1>}
         <Form>
           <Row className={classes.formContainer}>
-            <Form.Group className={classes.formGroup} controlId='user-name'>
+            {pageType === 'signup' && <Form.Group className={classes.formGroup} controlId='user-name'>
               <Form.Label>Name</Form.Label>
               <Form.Control type='text' />
-            </Form.Group>
+            </Form.Group>}
             <Form.Group className={classes.formGroup} controlId='user-email'>
               <Form.Label>Email</Form.Label>
               <InputGroup>
@@ -25,11 +31,18 @@ const Login = () => {
                 <Form.Control type='email' />
               </InputGroup>
             </Form.Group>
-            <Form.Group className={classes.formGroup} controlId='user-password'>
+            {(pageType === 'signup' || pageType === 'login') && <Form.Group className={classes.formGroup} controlId='user-password'>
               <Form.Label>Password</Form.Label>
               <Form.Control type='password' />
-            </Form.Group>
-            <Button type='submit' className={classes.submitButton}>Login</Button>
+            </Form.Group>}
+            <div className={classes.toggleContainer}>
+              {pageType === 'login' && <Link onClick={() => loginToggeler('signup')}>Create an account</Link>}
+              {pageType === 'login' && <Link onClick={() => loginToggeler('reset-form')}>Forgot Password?</Link>}
+              {pageType === 'signup' && <Link onClick={() => loginToggeler('login')}>Already have an account?</Link>}
+            </div>
+            {pageType === 'login' && <Button type='submit' className={classes.submitButton}>Login</Button>}
+            {pageType === 'signup' && <Button type='submit' className={classes.submitButton}>Signup</Button>}
+            {pageType === 'reset-form' && <Button type='submit' className={classes.submitButton}>Send email</Button>}
           </Row>
         </Form>
       </div>
